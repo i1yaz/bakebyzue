37ab95e65738fe364e4c88c2de1be6e4887b9ff7
Muhammad Ilyas <danish.ilyas786@gmail.com>
2026-05-09 06:41:19 +0500

    fix design

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        config(['livewire.temporary_file_upload.disk' => 'local']);
     }
 }

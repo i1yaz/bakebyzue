7dc515fe08a3cad769e924473eab88d53ea05138
Muhammad Ilyas <danish.ilyas786@gmail.com>
2026-05-09 05:49:00 +0500

    fix design

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -49,7 +50,8 @@ class User extends Authenticatable
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return true;
-        return in_array($this->email,  ['bakebyzue@gmail.com']);
+        return in_array($this->email, [
+            'bakebyzue@gmail.com',
+        ]) || app()->environment('local');
     }
 }
